9d48265a03ab998869dcce0b715cd73e844d3e3e
zehranrgi <zehranr3599@gmail.com>
2023-07-15 23:12:51 -0700

    lesson2added

---
+++ lesson2/ransac/build/CMakeFiles/quizRansac.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/zehranrgi/Downloads/DeepLearning/sensor_fusion_udacity/SFND_Lidar_Obstacle_Detection/src/render/render.cpp" "CMakeFiles/quizRansac.dir/Users/zehranrgi/Downloads/DeepLearning/sensor_fusion_udacity/SFND_Lidar_Obstacle_Detection/src/render/render.cpp.o" "gcc" "CMakeFiles/quizRansac.dir/Users/zehranrgi/Downloads/DeepLearning/sensor_fusion_udacity/SFND_Lidar_Obstacle_Detection/src/render/render.cpp.o.d"
+  "/Users/zehranrgi/Downloads/DeepLearning/sensor_fusion_udacity/SFND_Lidar_Obstacle_Detection/src/quiz/ransac/ransac3d_edit.cpp" "CMakeFiles/quizRansac.dir/ransac3d_edit.cpp.o" "gcc" "CMakeFiles/quizRansac.dir/ransac3d_edit.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lesson2/ransac/build/CMakeFiles/quizRansac.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quizRansac.dir/Users/zehranrgi/Downloads/DeepLearning/sensor_fusion_udacity/SFND_Lidar_Obstacle_Detection/src/render/render.cpp.o"
+  "CMakeFiles/quizRansac.dir/Users/zehranrgi/Downloads/DeepLearning/sensor_fusion_udacity/SFND_Lidar_Obstacle_Detection/src/render/render.cpp.o.d"
+  "CMakeFiles/quizRansac.dir/ransac3d_edit.cpp.o"
+  "CMakeFiles/quizRansac.dir/ransac3d_edit.cpp.o.d"
+  "quizRansac"
+  "quizRansac.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quizRansac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
